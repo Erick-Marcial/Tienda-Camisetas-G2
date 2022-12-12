4d345fead132bc996a5ee49186d4f69edc81e395
Erick Marcial Machacado Rizo <erick_marcialm@hotmail.com>
2022-12-11 22:49:32 -0500

    Actualización del Proyecto
    Actualización e incorporación del FrontEnd

--- tiendac3s/src/main/java/com/example/demo/Controllers/HolaController.java
+++ tiendac3s/src/main/java/com/example/demo/Controllers/HolaController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HolaController {
-    @GetMapping("/")
+    @GetMapping("/hola")
     public String hola(){
         return "caso prueba 2 **----hola nivelacion ciclo 3!!!";
     }
